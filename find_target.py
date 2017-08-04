645f4f99fffc7a9abdeaacf6a22ddc37d7f767ae
pythoncat1024 <pythoncat@qq.com>
2017-08-04 23:38:59 +0800

    Update find_target.py
    之前少了至关重要的一步：切换目录！

--- find_target.py
+++ find_target.py
@@ -5,6 +5,8 @@
 # @date   : 2017/8/2.
 
 import os
+import multiprocessing
+import time
 
 
 def bash_shell(bash_command):
@@ -14,8 +16,9 @@ def bash_shell(bash_command):
     :return: bash 命令执行后的控制台输出
     """
     try:
+        print("bash_command = {}".format(bash_command))
         return os.popen(bash_command).read().strip()
-    except:
+    except Exception:
         return None
 
 
@@ -30,19 +33,47 @@ def find_target(target_path="./../", key='.git'):
     for super_dir, dir_names, file_names in walk:
         for dir_name in dir_names:
             if dir_name == key:
-                dir_full_path = os.path.join(super_dir, dir_name)
-                # print(dir_full_path, super_dir, dir_name, sep=" ## ")
                 yield super_dir
 
 
-if __name__ == '__main__':
-    for repo_path in find_target('/Users/cat/Desktop/testGit/a6', key='.repo'):
-        print('find repo in -->', repo_path)
+def execute(semaphore, msg, command='ls -a', path="./"):
+    try:
+        semaphore.acquire()
+        # print(msg)
+        # time.sleep(1)
+        print(bash_shell(command))
+    finally:
+        semaphore.release()
 
-    for git_path in find_target('/Users/cat/Desktop/testGit/a6', key='.git'):
-        print('find git in -->', git_path)
-        # print(bash_shell('git config --global core.filemode false'))
-    # test for bash_command
-    # print(bash_shell('git init'))
+
+if __name__ == '__main__':
+    s_time = time.time()
+
+    s = multiprocessing.Semaphore(10)
+
+    command_path = '/Users/cat/Desktop/testGit/a6'
+    # target_path = '/'
+    for repo_path in find_target(command_path, key='.repo'):
+        os.chdir(repo_path)  # todo: the key code !!!
+        msg = 'find repo in -->{}'.format(repo_path)
+        p = multiprocessing.Process(target=execute,
+                                    args=(s, msg,),
+                                    kwargs={'command': 'pwd', 'path': repo_path})
+        p.daemon = True
+        p.start()
+        p.join()
+    for git_path in find_target(command_path, key='.git'):
+        os.chdir(git_path)
+        msg = 'find git in -->{}'.format(git_path)
+        p = multiprocessing.Process(target=execute,
+                                    args=(s, msg,),
+                                    kwargs={'command': 'pwd', 'path': git_path})
+        p.daemon = True
+        p.start()
+        p.join()
+    e_time = time.time()
+
+    print("spend time : {:.3f} seconds".format(e_time - s_time))
+    print('end ', "#" * 20, " end")
     # print(bash_shell('ls -al'))
     pass
